a0d41b181bffb05b838baec5964e2f78105b668a
Renn F <rennf93@gmail.com>
2022-10-23 15:02:31 +0200

    Fixing linting.

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -1,7 +1,7 @@
 """
 Tests for the Django admin modifications.
 """
-
+# noqa
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
@@ -15,12 +15,12 @@ class AdminSiteTests(TestCase):
         """Create user and client."""
 
         self.client = Client()
-        self.admin_user = get_user_model().objects.create_superuser( # type: ignore
+        self.admin_user = get_user_model().objects.create_superuser(  # type: ignore
             email='admin@example.com',
             password='testpass123',
         )
         self.client.force_login(self.admin_user)
-        self.user = get_user_model().objects.create_user( # type: ignore
+        self.user = get_user_model().objects.create_user(  # type: ignore
             email='user@example.com',
             password='testpass123',
             name='Test User'
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -32,7 +32,7 @@ class ModelTests(TestCase):
             ['test4@example.COM', 'test4@example.com'],
         ]
         for email, expected in sample_emails:
-            user = get_user_model().objects.create_user( # type: ignore
+            user = get_user_model().objects.create_user(  # type: ignore
                 email, 'sample123'
                 )
 
